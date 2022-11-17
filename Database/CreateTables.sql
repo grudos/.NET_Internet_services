ef3317df9b8931ada49255e9c8c02b2a5714a0b8
grudos <jg04@wp.pl>
2022-11-17 11:45:17 +0100

    Corrected sql script

--- Database/CreateTables.sql
+++ Database/CreateTables.sql
@@ -3,7 +3,7 @@ use SportsAgents;
 create table Users(
 	[Login] nvarchar(128) not null,
 	[Password] nvarchar(255) not null,
-	EMail nvarchar(100) not null,
+	EMail nvarchar(128) not null,
     FullName nvarchar(40) not null,
     Age int not null,
 	primary key ([Login])
@@ -21,13 +21,19 @@ create table Athletes(
 );
 
 insert into Users([Login], [Password], EMail, FullName, Age)
-values('user1', 'user1', 's180319@student.pg.edu.pl', 'user userowski', 22);
+values('admin@student.pg.edu.pl', 'admin', 'admin@student.pg.edu.pl', 'admin admin', 30);
 
 insert into Users([Login], [Password], EMail, FullName, Age)
-values('admin', 'admin', 'admin@student.pg.edu.pl', 'admin admin', 30);
+values('user1@example.com', 'user1', 'user1@example.com', 'user user', 30);
 
 insert into Athletes(FullName, Age, DisciplineName, UserLogin)
-values('Robert Kubica', 35, 'Formula 1', 'user1');
+values('Robert Kubica', 35, 'Formula 1', 'user1@example.com');
+
+insert into Athletes(FullName, Age, DisciplineName, UserLogin)
+values('Bartosz Zmarlik', 35, '¯uzel', 'user1@example.com');
+
+insert into Athletes(FullName, Age, DisciplineName, UserLogin)
+values('Robert Lewandowski', 35, 'Pi³ka No¿na', 'user1@example.com');
 
 /*
 drop table if exists Athletes
